c72016f49e6cb0f6b12312148b0d8a0ecab9e99c
Edem Kwaku <avorleyedemkwaku@gmail.com>
2022-12-19 17:32:40 -0800

    Added 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,27 +1,18 @@
-/**
+/*
  * File: 3-puts.c
-*/
-
- #include "main.h"
-
-
-/**
-
- * _puts - Prints a string to stdout.
-
- * @str: The string to be printed.
-
+ * Auth: Brennan D Baraban
  */
 
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+ */
 void _puts(char *str)
-
 {
-
 	while (*str)
-
 		_putchar(*str++);
 
-
 	_putchar('\n');
-
 }
