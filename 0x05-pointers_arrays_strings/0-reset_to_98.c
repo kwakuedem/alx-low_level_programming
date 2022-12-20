ef141b8724b395791ea224254c6d40bf20cbda30
Edem Kwaku <avorleyedemkwaku@gmail.com>
2022-12-19 16:31:36 -0800

    Added 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,5 @@
 /* * File: 0-reset_to_98.c */
-#include "holberton.h"
+#include "main.h"
 
 /** * reset_to_98 - Takes a pointer to an int and updates * the value it points to to 
  98. * @n: The pointer to an int. */
