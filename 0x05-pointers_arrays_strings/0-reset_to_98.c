e9a3de82d6eddfb994958d15330e911f5dd16d56
Edem Kwaku <avorleyedemkwaku@gmail.com>
2022-12-19 17:13:08 -0800

    Added 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,15 @@
-/* * File: 0-reset_to_98.c */
+/*
+ * File: 0-reset_to_98
+*/
+
 #include "main.h"
 
-/** * reset_to_98 - Takes a pointer to an int and updates * the value it points to to 
- 98. * @n: The pointer to an int. */
-
-void reset_to_98(int *n) { *n = 98;
+/**
+ * reset_to_98 - Takes a pointer to an int and updates
+ *               the value it points to to 98.
+ * @n: The pointer to an int.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
