c3463d5f9b60b5efe9e4f05d5d1cefc035a28e36
Edem Kwaku <avorleyedemkwaku@gmail.com>
2022-12-23 21:29:44 -0800

    Added 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,6 @@
 /*
  * File: 0-strcat.c
- * Auth: Brennan D Baraban
- */
+*/
 
 #include "main.h"
 
@@ -14,7 +13,7 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char *strcat(char *dest, const char *src)
+char *strcat(char *dest,char *src)
 {
 	int index = 0, dest_len = 0;
 
