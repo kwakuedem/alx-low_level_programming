3e84a88b56127147b4d160e293515fd19ac2695f
Avorley Edem Kwaku <edemkwakuavorley@gmail.com>
2023-01-18 17:03:27 -0800

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,29 +10,33 @@
   */
 int main(int argc, char *argv[])
 {
-	ud_t ud_obj;
-	int val = 0, i = 0;
-
-	if (argc == 2)
+	int bytes, i;
+	char *arr;
+	
+	if (argc != 2)
 	{
-		val = atoi(argv[1]);
-
-		if (val < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
-		}
+		printf("error\n");
+		exit(1);
 	}
+	
+	bytes=atoi(argv[i]);
 
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	
+	arr=(char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n" arr[i]);
+			break;
+		}
+		printf("%02hhx\n", arr[i]);
+	}
 	return (0);
 }
